8c9e8f4e01959f0d41d50b0dd9e6a0138bba6b16
Sonya P <133888064+maybemountains@users.noreply.github.com>
2024-12-23 00:54:45 +0100

    created the queer routes
    including adding all assets for the informative route and for the queer people route

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/informative')
+def informative():
+    return render_template('informative.html')
+
+@app.route('/queerPeople')
+def queer_people():
+    return render_template('queerPeople.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
