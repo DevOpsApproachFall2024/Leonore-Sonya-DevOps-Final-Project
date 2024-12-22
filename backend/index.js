6969c91a1e38b38c022fec87e3c65becc048498e
LeonoreNdebele <127037813+LeonoreNdebele@users.noreply.github.com>
2024-12-22 22:49:36 +0100

    chore: hot fix index file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // anything we do that is math related will be here. 
 // we can make it respond with a link so that everything else isn't soley on the front end, this can be discussed. 
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-//factotial calculator route
+//factorial calculator route
 app.get('/factorial/:num', function(req, res) {
   let {num} = req.params;
 
@@ -38,3 +35,5 @@ app.get('/factorial/:num', function(req, res) {
 // Start Server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
