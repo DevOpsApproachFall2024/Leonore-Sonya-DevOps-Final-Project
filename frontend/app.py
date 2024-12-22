0f6e5876df2c89d3d772fd78af96e69b082d3352
Sonya P <133888064+maybemountains@users.noreply.github.com>
2024-12-22 23:45:11 +0100

    added tony route and styling

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/tony')
+def tony():
+    return render_template('tony.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
