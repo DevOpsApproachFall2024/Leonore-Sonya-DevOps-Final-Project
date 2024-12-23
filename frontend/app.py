946c06bcea2bb01d90477b317be6d7b2c21027d8
Sonya P <133888064+maybemountains@users.noreply.github.com>
2024-12-23 01:39:38 +0100

    complete easter egg routes

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/never')
+def never():
+    return render_template('giveup.html')
+
+@app.route('/plstwenty')
+def twenty():
+    return render_template('plstwenty.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
